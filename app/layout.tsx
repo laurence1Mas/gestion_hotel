8e2e9ca7f2bb97915c6aac6961f2d0c30f8690c1
laurence1Mas <laurencemasika01@gmail.com>
2026-04-04 21:34:11 +0200

    fix: clean the page and layout files in the app folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/app/components/navbar";
-import {Footer} from "@/app/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,9 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar />
         {children}
-        <Footer/>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 
-import Hero from "@/app/components/Hero";
 export default function Home() {
   return (
     <div>
       <main>
-        <Hero/>
       </main>
     </div>
   );
