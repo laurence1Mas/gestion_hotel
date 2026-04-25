3a055ad4856b5035730d222c98e845f5f2668c15
Grady243 <gradyaganzemasirika@gmail.com>
2026-04-25 18:24:44 +0300

    feat:update hero section

--- components/header.tsx
+++ components/header.tsx
@@ -8,15 +8,15 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Menu, User, X } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import Image from "next/image";
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b border-border">
+    <header className="sticky top-0 z-50 w-full py-2  bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b border-border">
       <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
         <Link href="/" className="flex items-center gap-2">
           <Image
@@ -61,7 +61,7 @@ export function Header() {
             <DropdownMenuTrigger asChild>
               <Link href="/auth/login">
                 <Button variant="outline">
-                  <User className="w-4 h-4 mr-2" />
+                  <User className="text-base hover:text-primary/90" />
                   Connexion
                 </Button>
               </Link>
