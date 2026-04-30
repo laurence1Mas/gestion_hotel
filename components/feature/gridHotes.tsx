de3df96e4994404d47b672cbc296f6ad00abb49c
Alexs Kapene <alexskapene2003@gmail.com>
2026-04-29 19:41:22 -0700

    feat: add component listeHotel

---
+++ components/feature/gridHotes.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent } from "../ui/card";
+import { Button } from "../ui/button";
+
+type Hotels = {
+    id:number;
+    name:string;
+    
+}
+
+export function GridHotels (){
+    return ("Grid Hotels")
+}
